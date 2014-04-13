3946815d471bf661514389d60190f5d0695456ba
George Ehrhorn <ehrhorn@gmail.com>
2014-04-12 20:47:20 -0400

    Added aliases

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -18,7 +18,7 @@ alias history='fc -l 1'
 # List direcory contents
 alias lsa='ls -lahp'
 alias l='ls -lA1p'
-alias ll='ls -alhp'
+alias ll='ls -lhp'
 alias la='ls -lAhdp .*'
 alias sl=ls # often screw this up
 
